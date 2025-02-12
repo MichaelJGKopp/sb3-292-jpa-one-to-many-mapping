99d31227d384c65a7864c94e2a8adb33c47fbdbe
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-02-12 19:49:13 +0100

    add another join fetch for instructor_detail to reduce queries

--- src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
+++ src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
@@ -38,7 +38,10 @@ public class AppDAOImpl implements AppDAO {
     public Instructor findInstructorByIdJoinFetch(int theId) {
 
         TypedQuery<Instructor> query = entityManager.createQuery(
-                "select i from Instructor i JOIN FETCH i.courses where i.id=:data", Instructor.class);
+                "select i from Instructor i " +
+                        "JOIN FETCH i.courses " +
+                        "JOIN FETCH i.instructorDetail " +
+                        "where i.id=:data", Instructor.class);
         query.setParameter("data", theId);
 
         return query.getSingleResult();
