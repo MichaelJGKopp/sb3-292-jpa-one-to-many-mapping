eb80d336472c98a22d99ac18d04b504320459c44
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-02-16 11:29:05 +0100

    add student-course join table

--- sql/hb-05-many-to-many/create-db.sql
+++ sql/hb-05-many-to-many/create-db.sql
@@ -1,12 +1,10 @@
-DROP SCHEMA IF EXISTS `hb-03-one-to-many`;
+DROP SCHEMA IF EXISTS `hb-05-many-to-many`;
 
-CREATE SCHEMA `hb-03-one-to-many`;
+CREATE SCHEMA `hb-05-many-to-many`;
 
-use `hb-03-one-to-many`;
+use `hb-05-many-to-many`;
 
 SET FOREIGN_KEY_CHECKS = 0;
-
-DROP TABLE IF EXISTS `instructor_detail`;
 
 CREATE TABLE `instructor_detail` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -15,8 +13,6 @@ CREATE TABLE `instructor_detail` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-
-DROP TABLE IF EXISTS `instructor`;
 
 CREATE TABLE `instructor` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -30,7 +26,6 @@ CREATE TABLE `instructor` (
   REFERENCES `instructor_detail` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `course`;
 
 CREATE TABLE `course` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -50,27 +45,48 @@ CREATE TABLE `course` (
   ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
-SET FOREIGN_KEY_CHECKS = 1;
-
-# Add review table
-DROP TABLE IF EXISTS `review`;
-
-SET FOREIGN_KEY_CHECKS = 0;
 
 CREATE TABLE `review` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `comment` VARCHAR(256) DEFAULT NULL,
-    `course_id` INT DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `comment` varchar(256) DEFAULT NULL,
+  `course_id` int DEFAULT NULL,
 
-    PRIMARY KEY(`id`),
+  PRIMARY KEY (`id`),
 
-    KEY `FK_COURSE_ID_idx` (`course_id`),
+  KEY `FK_COURSE_ID_idx` (`course_id`),
 
-    CONSTRAINT `FK_COURSE_ID`
-    FOREIGN KEY (`course_id`)
-    REFERENCES `course`(`id`)
+  CONSTRAINT `FK_COURSE` 
+  FOREIGN KEY (`course_id`) 
+  REFERENCES `course` (`id`) 
 
-    ON DELETE NO ACTION ON UPDATE NO ACTION
-) engine = InnoDB default charset = latin1;
+  ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `student` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `course_student` (
+  `course_id` int NOT NULL,
+  `student_id` int NOT NULL,
+  
+  PRIMARY KEY (`course_id`,`student_id`),
+  
+  KEY `FK_STUDENT_idx` (`student_id`),
+  
+  CONSTRAINT `FK_COURSE_05` FOREIGN KEY (`course_id`) 
+  REFERENCES `course` (`id`) 
+  ON DELETE NO ACTION ON UPDATE NO ACTION,
+  
+  CONSTRAINT `FK_STUDENT` FOREIGN KEY (`student_id`) 
+  REFERENCES `student` (`id`) 
+  ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 SET FOREIGN_KEY_CHECKS = 1;
